6ac3e5ee49bc01aac6cfb28ea5811f23ccd724dd
Haixun Wang <haixun@gmail.com>
2023-09-04 13:15:35 -0700

    auto

---
+++ readinglist.py
@@ -0,0 +1,17 @@
+import json
+
+with open('readinglist.json', 'r') as file:
+    data = json.load(file)
+
+with open('readinglist.markdown', 'w') as publicReadingList, open('myreadinglist.markdown', 'w') as personalReadingList:
+    publicReadingList.write(f"# Reading List\n\n")
+    personalReadingList.write(f"# Reading List\n\n")
+
+    for week, articles in data.items():
+        publicReadingList.write(f"### Week of {week}\n\n")
+        personalReadingList.write(f"### Week of {week}\n\n")
+        for article in articles:
+            publicReadingList.write(f"* [{article['title']}]({article['url']}), {article['source']}\n")
+            personalReadingList.write(f"* [{article['title']}]({article['url']}), [Local]({article['local']}), {article['source']}\n")
+        publicReadingList.write(f"\n")
+        personalReadingList.write(f"\n")
